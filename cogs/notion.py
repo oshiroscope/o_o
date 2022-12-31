d3e44bf16fbf6baefc1c18c68b313c3bb0c9e637
oshiroscope <shiro@hongoproduction.com>
2023-01-01 02:31:27 +0900

    add notion registration and hot reload

--- cogs/notion.py
+++ cogs/notion.py
@@ -2,21 +2,56 @@ from discord import app_commands
 from discord.ext import commands
 import discord
 import os
-
-
+import requests
 
 class Notion(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @commands.hybrid_command()
-    async def fuga(self, ctx):
-        await ctx.send("テスト成功！")
+    @app_commands.command()
+    async def inbox(self, interaction: discord.Interaction, title: str) -> None:
+        url = "https://api.notion.com/v1/pages"
 
-async def setup(bot):
-    if os.path.isfile('.env'):
-        from dotenv import load_dotenv
-        load_dotenv()
+        NOTION_API_KEY = os.environ['NOTION_API_KEY']
+        NOTION_DATABASE_ID = os.environ['NOTION_DATABASE_ID']
+        emoji = "😐"
+        headers = {
+            "Accept": "application/json",
+            "Notion-Version": "2022-06-28",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + NOTION_API_KEY,
+        }
+
+        payload = {
+            "parent": {"database_id": NOTION_DATABASE_ID},
+            "icon": {"emoji": emoji},
+            "properties": {
+                "Name": {
+                    "title": [
+                        {
+                            "text": {"content": title},
+                        }
+                    ],
+                },
+            },
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+        result_dict = response.json()
+        result = result_dict["object"]
+        page_url = result_dict["url"]
+        page_title = result_dict["properties"]["Name"]["title"][0]["text"]["content"]
+
+        message = ""
+        if result == "page":
+            message = "「" + page_title + "」が作成されたよ～ XXXX!!\n " + page_url
+        elif result == "error":
+            message = "なんかエラーが発生しているみたい！\n " + page_url
+        else:
+            message = ("例外起きて草。なんも分からん。\n " + page_url,)
+
+        await interaction.response.send_message(message)
+
+async def setup(bot: commands.Bot) -> None:
     MY_GUILD = discord.Object(id=int(os.environ['DISCORD_GUILD_ID']))
-    # await bot.add_cog(Notion(bot)) 
-    await bot.add_cog(Notion(bot), guilds=MY_GUILD) 
+    await bot.add_cog(Notion(bot), guild=MY_GUILD) 
--- o_o.py
+++ o_o.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
+
+from discord.ext.commands import Greedy, Context
+from typing import Literal, Optional
+
 import os
 import traceback
 
@@ -17,15 +22,12 @@ class o_o(commands.Bot):
             load_dotenv()
         self.MY_GUILD = discord.Object(id=int(os.environ['DISCORD_GUILD_ID']))
 
-    
     async def setup_hook(self):
         for cog in cogslist:
             try: 
                 await self.load_extension(cog)
             except Exception:
                 traceback.print_exc()
-        # tree = await self.tree.sync()
-        # print(tree)
 
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
@@ -34,67 +36,16 @@ class o_o(commands.Bot):
         await self.tree.sync(guild=self.MY_GUILD)
 
 bot = o_o()
+
+@bot.tree.command(name='reload')
+async def reload(interaction: discord.Interaction):
+    print('reloading...')
+    for cog in cogslist:
+        try: 
+            await bot.reload_extension(cog)
+        except Exception:
+            traceback.print_exc()
+    await interaction.response.send_message('reloading success!')
+
 bot.run(os.environ['DISCORD_BOT_TOKEN'])        
 
-
-# @bot.slash_command(guild_ids=[DISCORD_GUILD_ID])
-# async def hello(
-#     ctx,
-#     name: Option(str, "名前を入力してください"),
-#     gender: Option(str, "性別を選択してください", choices=["男性", "女性", "その他"]),
-#     age: Option(int, "年齢を入力してください", required=False, default=18),
-# ):
-#     await ctx.respond(f"こんにちは、{name}さん")
-
-
-# @bot.slash_command(guild_ids=[DISCORD_GUILD_ID])
-# async def notion(
-#     ctx,
-#     title: Option(str, "名前を入力してください"),
-# ):
-
-#     url = "https://api.notion.com/v1/pages"
-
-#     print("notion_api_key" + NOTION_API_KEY)
-#     api_key = NOTION_API_KEY
-#     database_id = NOTION_DATABASE_ID
-#     emoji = "🤠"
-#     headers = {
-#         "Accept": "application/json",
-#         "Notion-Version": "2022-06-28",
-#         "Content-Type": "application/json",
-#         "Authorization": "Bearer " + api_key,
-#     }
-
-#     payload = {
-#         "parent": {"database_id": database_id},
-#         "icon": {"emoji": emoji},
-#         "properties": {
-#             "Name": {
-#                 "title": [
-#                     {
-#                         "text": {"content": name},
-#                     }
-#                 ],
-#             },
-#         },
-#     }
-
-#     response = requests.post(url, json=payload, headers=headers)
-#     result_dict = response.json()
-#     result = result_dict["object"]
-#     page_url = result_dict["url"]
-#     page_title = result_dict["properties"]["Name"]["title"][0]["text"]["content"]
-#     print(page_title)
-
-#     message = ""
-#     if result == "page":
-#         message = "「" + page_title + "」が作成されたよ～！！\n " + page_url
-#     elif result == "error":
-#         message = "なんかエラーが発生しているみたい！\n " + page_url
-#     else:
-#         message = ("例外起きて草。なんも分からん。\n " + page_url,)
-
-#     await ctx.respond(message)
-
-# bot.run(DISCORD_BOT_TOKEN)
